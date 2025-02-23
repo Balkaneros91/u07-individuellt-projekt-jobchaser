9669c3cb508d22f06f3d2bd0f15ea62d1f288183
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2025-02-23 23:01:50 +0100

    Fetch data add list for jobs and initiate job item

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,11 +1,42 @@
 "use client";
 
-import React from 'react'
+import JobList from "@/components/JobList";
+import { useState, useEffect } from "react";
 
 const JobsPage = () => {
-  return (
-    <div>JobsPage</div>
-  )
+
+    const[jobs, setJobs] = useState([]);
+    const[loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+          setLoading(true);
+          try {
+            const response = await fetch("https://jsonfakery.com/jobs");
+            const data = await response.json();
+            setJobs(Array.isArray(data) ? data : data.jobs || []);
+          } catch (error) {
+            console.error(`Error fetching jobs: ${error}`);
+          } finally {
+            setLoading(false);
+          }
+        };
+    
+        fetchJobs();
+      }, []);
+    
+      return (
+        <div className="grid grid-cols-1 gap-6 min-h-screen px-8 py-20">
+          <h1 className="text-2xl font-bold">Job Listings</h1>
+          {loading ? (
+            <p>Loading...</p>
+          ) : jobs.length > 0 ? (
+            <JobList jobs={jobs}/>
+          ) : (
+            <p>No jobs available.</p>
+          )}
+        </div>
+      );
 }
 
 export default JobsPage
--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,9 +1,35 @@
 "use client";
 
-const JobList = () => {
-  return (
-    <div>JobList</div>
-  )
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+
+import { Job } from "./Job";
+import JobItem from "./JobItem";
+
+interface JobListProps {
+  jobs: Job[];
 }
+
+const JobList:React.FC<JobListProps> = ({jobs}) => {
+    const searchTerm = useSelector((state: RootState) => state.searchTerm); 
+
+    const filteredJobs = jobs.filter((job: Job) =>
+        job.title.toLowerCase().includes(searchTerm.toLowerCase()) || 
+        job.location.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+  if(filteredJobs.length === 0) {
+    return <p>No jobs available</p>;
+  }
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <JobItem />
+      </div>
+    </div>
+    
+  );
+};
 
 export default JobList
