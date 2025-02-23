a722ee6afea1c7ee23e67a80931a78942b9282db
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2025-02-23 23:23:42 +0100

    Add redux toolkit logic to my data fetch

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,42 +1,46 @@
 "use client";
 
 import JobList from "@/components/JobList";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setJobs, setLoading } from "@/redux/jobSlice";
+import { RootState } from "@/redux/store";
 
 const JobsPage = () => {
 
-    const[jobs, setJobs] = useState([]);
-    const[loading, setLoading] = useState(true);
+    const dispatch = useDispatch();
+
+    const { jobs, filteredJobs, loading } = useSelector((state: RootState) => state.jobs);
+
 
     useEffect(() => {
         const fetchJobs = async () => {
-          setLoading(true);
-          try {
-            const response = await fetch("https://jsonfakery.com/jobs");
-            const data = await response.json();
-            setJobs(Array.isArray(data) ? data : data.jobs || []);
-          } catch (error) {
-            console.error(`Error fetching jobs: ${error}`);
-          } finally {
-            setLoading(false);
-          }
+            dispatch(setLoading(true));
+                    try {
+                const response = await fetch("https://jsonfakery.com/jobs");
+                const data = await response.json();
+                dispatch(setJobs(Array.isArray(data) ? data : data.jobs || []));
+            } catch (error) {
+                console.error(`Error fetching jobs: ${error}`);
+            } finally {
+                dispatch(setLoading(false));
+            }
         };
-    
         fetchJobs();
-      }, []);
+    }, [dispatch]);
     
-      return (
-        <div className="grid grid-cols-1 gap-6 min-h-screen px-8 py-20">
-          <h1 className="text-2xl font-bold">Job Listings</h1>
-          {loading ? (
-            <p>Loading...</p>
-          ) : jobs.length > 0 ? (
-            <JobList jobs={jobs}/>
-          ) : (
-            <p>No jobs available.</p>
-          )}
-        </div>
-      );
+  return (
+    <div className="grid grid-cols-1 gap-6 min-h-screen px-8 py-20">
+      <h1 className="text-2xl font-bold">Job Listings</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : jobs.length > 0 ? (
+        <JobList jobs={filteredJobs} />
+      ) : (
+        <p>No jobs available.</p>
+      )}
+    </div>
+  );
 }
 
 export default JobsPage
